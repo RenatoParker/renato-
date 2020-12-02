8c2d78d5377037a22650798a111aa6da9a09ed97
renatoambrosone <s279186@studenti.polito.it>
2020-12-03 00:14:05 +0100

    animated_loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,80 +7,84 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Alert from 'react-bootstrap/Alert'
 
-class IndexPage extends React.Component{
-  constructor(props){
+class IndexPage extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {shown: false}
+    this.state = { shown: false }
     this.handleShown = this.handleShown.bind(this)
 
   }
 
-  handleShown = () =>{
+  handleShown = () => {
     console.log("shown:" + this.state.shown)
     let temp = this.state.shown ? false : true
-    this.setState({shown: temp})
+    this.setState({ shown: temp })
   }
 
 
 
 
 
-// modifyShown = () =>{
-//   console.log("ciao");
-//  // shown = shown ? false : true;
-// }
+  // modifyShown = () =>{
+  //   console.log("ciao");
+  //  // shown = shown ? false : true;
+  // }
 
-render () {
-  return(
+  render() {
+    return (
 
-  
+
       <Layout>
-       
+
         <SEO title="-renato-" />
 
         <h1>Hi this is a test with gatsby</h1>
         <p>Actually I added bootstrap!</p>
-        <ButtonTest onClick={this.handleShown} variant="primary">Primary</ButtonTest>{' '}
-        
-        
+        <ButtonTest onClick={this.handleShown} variant="primary">CLICCA!!!</ButtonTest>{' '}
+
+
+
         {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
           <Image />
     
     
         </div>*/}
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-        <Alert show = {this.state.shown} key='first_alert' variant='success'>
-        Bravo, sai premere un pulsante!
+
+        <Alert show={this.state.shown} key='first_alert' variant='warning'>
+          Sto ancora lavorando! Ecco a te un bellisssimo caricamento:
       </Alert>
+        {this.state.shown ? <div show={this.state.shown} class="loader"></div> : <></>}
+        <div>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link></div>
       </Layout>
-    
-
-  )
-}
 
 
-// const IndexPage = () => (
-  
-//   <Layout>
-   
-//     <SEO title="-renato-" />
-//     <Alert show = {shown} key='first_alert' variant='primary'>
-//     This is a test alert—check it out!
-//   </Alert>
-//     <h1>Hi this is a test with gatsby</h1>
-//     <p>Actually I added bootstrap!</p>
-//     <ButtonTest onclick={test2()} variant="primary">Primary</ButtonTest>{' '}
-//     <button onclick = {test}>ciao</button>
-    
-//     {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-//       <Image />
+    )
+  }
 
 
-//     </div>*/}
-//     <Link to="/page-2/">Go to page 2</Link> <br />
-//     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-//   </Layout>
-// )
+  // const IndexPage = () => (
+
+  //   <Layout>
+
+  //     <SEO title="-renato-" />
+  //     <Alert show = {shown} key='first_alert' variant='primary'>
+  //     This is a test alert—check it out!
+  //   </Alert>
+  //     <h1>Hi this is a test with gatsby</h1>
+  //     <p>Actually I added bootstrap!</p>
+  //     <ButtonTest onclick={test2()} variant="primary">Primary</ButtonTest>{' '}
+  //     <button onclick = {test}>ciao</button>
+
+  //     {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  //       <Image />
+
+
+  //     </div>*/}
+  //     <Link to="/page-2/">Go to page 2</Link> <br />
+  //     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  //   </Layout>
+  // )
 }
 export default IndexPage
